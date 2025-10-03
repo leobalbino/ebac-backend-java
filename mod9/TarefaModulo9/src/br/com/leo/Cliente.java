f7c8c9335c972ff642929b9a2a1ad89fe3b27fee
Leonardo Balbino <leobalbino98@gmail.com>
2025-10-03 15:13:51 -0300

    Adicionando projeto do Módulo 9

---
+++ mod9/TarefaModulo9/src/br/com/leo/Cliente.java
@@ -0,0 +1,33 @@
+package br.com.leo;
+
+public class Cliente {
+
+    private String nome;
+
+    private Integer codigo;
+
+    private Long codigoMaior = 23423l;
+
+    private Double valorDecimal1 =10.1;
+
+    private Boolean status = false;
+
+    private Float valorDecimal = 10.0f;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character letra = 'A';
+
+    public static void main(String[] args) {
+
+    }
+
+    public void teste(){
+
+    }
+
+
+
+}
---
+++ mod9/TarefaModulo9/src/br/com/leo/ExercicioMod9.java
@@ -0,0 +1,16 @@
+package br.com.leo;
+
+public class ExercicioMod9 {
+
+
+    public static void main(String[] args) {
+
+         int numeroPrimitivo = 10;
+        System.out.println("Valor da variavel primitiva: " + numeroPrimitivo);
+
+        // Conversao para o tipo wrapper
+        Integer numeroWrapper = numeroPrimitivo;
+        System.out.println("Valor da variavel wrapper: " + numeroWrapper);
+
+    }
+}
